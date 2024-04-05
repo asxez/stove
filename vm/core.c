ed41752021b8d31b59b874e700fddcff77b4634d
asxe <2973918177@qq.com>
2024-04-05 22:20:16 +0800

    [update built-in class] update List class and it's method.

--- vm/core.c
+++ vm/core.c
@@ -921,6 +921,134 @@ static bool primStringToString(VM *vm UNUSED, Value *args) {
     RET_VALUE(args[0])
 }
 
+//objList.new()：创建一个列表
+static bool primListNew(VM *vm, Value *args UNUSED) {
+    RET_OBJ(newObjList(vm, 0))
+}
+
+//objList[_]：索引元素
+static bool primListSubScript(VM *vm, Value *args) {
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+
+    //数字和range对象
+    if (VALUE_IS_NUM(args[1])) {
+        uint32_t index = validateIndex(vm, args[1], objList->elements.count);
+        if (index == UINT32_MAX)
+            return false;
+        RET_VALUE(objList->elements.datas[index])
+    }
+
+    //range对象
+    if (!VALUE_IS_OBJRANGE(args[1]))
+        SET_ERROR_FALSE(vm, "subscript should be integer or range.")
+
+    int direction;
+    uint32_t count = objList->elements.count;
+    //返回的startIndex是objRange.from在objList.elements.data中的下标
+    uint32_t startIndex = calculateRange(vm, VALUE_TO_OBJRANGE(args[1]), &count, &direction);
+
+    //新建一个list存储该range在原来list中索引的元素
+    ObjList *result = newObjList(vm, count);
+    uint32_t idx;
+    for (idx = 0; idx < count; idx++)
+        result->elements.datas[idx] = objList->elements.datas[startIndex + idx * direction];
+    RET_OBJ(result)
+}
+
+//objList[_] = (_)：只支持数组作为索引
+static bool primListSubScriptSetter(VM *vm, Value *args) {
+    //获取对象
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+
+    //获取subscript
+    uint32_t index = validateIndex(vm, args[1], objList->elements.count);
+    if (index == UINT32_MAX)
+        return false;
+
+    //直接赋值
+    objList->elements.datas[index] = args[2];
+    RET_VALUE(args[2])
+}
+
+//objList.append(_)：列表末尾添加元素
+static bool primListAppend(VM *vm, Value *args) {
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+    ValueBufferAdd(vm, &objList->elements, args[1]);
+    RET_VALUE(args[0]) //把要添加的元素返回
+}
+
+//objList.appendCore_(_)：编译内部使用的，用于编译列表直接量
+static bool primListAppendCore(VM *vm, Value *args) {
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+    ValueBufferAdd(vm, &objList->elements, args[1]);
+    RET_VALUE(args[0]) //返回列表本身
+}
+
+//objList.clear()：清空列表
+static bool primListClear(VM *vm, Value *args) {
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+    ValueBufferClear(vm, &objList->elements);
+    RET_NULL
+}
+
+//objList.count：返回列表中元素个数
+static bool primListCount(VM *vm UNUSED, Value *args) {
+    RET_NUM(VALUE_TO_OBJLIST(args[0])->elements.count)
+}
+
+//objList.insert(_,_)：插入元素
+static bool primListInsert(VM *vm, Value *args) {
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+    //+1确保可以在最后插入
+    uint32_t index = validateIndex(vm, args[1], objList->elements.count + 1);
+    if (index == UINT32_MAX)
+        return false;
+    insertElement(vm, objList, index, args[2]);
+    RET_VALUE(args[2]) //把插入的数据返回
+}
+
+//objList.iterate(_)：迭代列表
+static bool primListIterate(VM *vm, Value *args) {
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+
+    //如果是第一次迭代，迭代索引肯定为空，直接返回索引0
+    if (VALUE_IS_NULL(args[1])) {
+        if (objList->elements.count == 0)
+            RET_FALSE
+        RET_NUM(0)
+    }
+
+    //确保迭代器是整数
+    if (!validateInt(vm, args[1]))
+        return false;
+    double iter = VALUE_TO_NUM(args[1]);
+    if (iter < 0 || iter >= objList->elements.count - 1)
+        RET_FALSE
+    RET_NUM(iter + 1) //返回下一个
+}
+
+//objList.iteratorValue(_)：返回迭代值
+static bool primListIteratorValue(VM *vm, Value *args) {
+    //获取实例对象
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+
+    uint32_t index = validateIndex(vm, args[1], objList->elements.count);
+    if (index == UINT32_MAX)
+        return false;
+    RET_VALUE(objList->elements.datas[index])
+}
+
+//objList.remove(_)：删除指定位置的元素
+static bool primListRemove(VM *vm, Value *args) {
+    //获取实例对象
+    ObjList *objList = VALUE_TO_OBJLIST(args[0]);
+
+    uint32_t index = validateIndex(vm, args[1], objList->elements.count);
+    if (index == UINT32_MAX)
+        return false;
+    RET_VALUE(removeElement(vm, objList, index)) //返回删除的值
+}
+
 //从modules中获取名为moduleName的模块
 static ObjModule *getModule(VM *vm, Value moduleName) {
     Value value = mapGet(vm->allModules, moduleName);
@@ -1187,4 +1315,18 @@ void buildCore(VM *vm) {
     PRIM_METHOD_BIND(vm->stringClass, "iteratorValue(_)", primStringIteratorValue)
     PRIM_METHOD_BIND(vm->stringClass, "toString", primStringToString)
     PRIM_METHOD_BIND(vm->stringClass, "count", primStringByteCount)
+
+    //绑定List类
+    vm->listClass = VALUE_TO_CLASS(getCoreClassValue(coreModule, "List"));
+    PRIM_METHOD_BIND(vm->listClass->objHeader.class, "new()", primListNew)
+    PRIM_METHOD_BIND(vm->listClass, "[_]", primListSubScript)
+    PRIM_METHOD_BIND(vm->listClass, "[_]=(_)", primListSubScriptSetter)
+    PRIM_METHOD_BIND(vm->listClass, "append(_)", primListAppend)
+    PRIM_METHOD_BIND(vm->listClass, "appendCore_(_)", primListAppendCore)
+    PRIM_METHOD_BIND(vm->listClass, "clear()", primListClear)
+    PRIM_METHOD_BIND(vm->listClass, "count", primListCount)
+    PRIM_METHOD_BIND(vm->listClass, "insert(_,_)", primListInsert)
+    PRIM_METHOD_BIND(vm->listClass, "iterate(_)", primListIterate)
+    PRIM_METHOD_BIND(vm->listClass, "iteratorValue(_)", primListIteratorValue)
+    PRIM_METHOD_BIND(vm->listClass, "remove(_)", primListRemove)
 }
