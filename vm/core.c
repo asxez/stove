9bffb665a6084ba40d8672749e338024a56bca70
asxe <2973918177@qq.com>
2024-04-06 00:55:32 +0800

    [update built-in class] update Map class and it's method.

--- vm/core.c
+++ vm/core.c
@@ -305,6 +305,21 @@ static int findString(ObjString *haystack, ObjString *needle) {
     return -1;
 }
 
+//校验key合法性
+static bool validateKey(VM *vm, Value arg) {
+    if (
+            VALUE_IS_TRUE(arg) ||
+            VALUE_IS_FALSE(arg) ||
+            VALUE_IS_NULL(arg) ||
+            VALUE_IS_NUM(arg) ||
+            VALUE_IS_OBJSTR(arg) ||
+            VALUE_IS_OBJRANGE(arg) ||
+            VALUE_IS_CLASS(arg)
+            )
+        return true;
+    SET_ERROR_FALSE(vm, "key must be value type.")
+}
+
 // !object: object取反，结果为false
 static bool primObjectNot(VM *vm UNUSED, Value *args) {
     RET_VALUE(VT_TO_VALUE(VT_FALSE))
@@ -1049,6 +1064,155 @@ static bool primListRemove(VM *vm, Value *args) {
     RET_VALUE(removeElement(vm, objList, index)) //返回删除的值
 }
 
+//objMap.new()：创建map对象
+static bool primMapNew(VM *vm, Value *args UNUSED) {
+    RET_OBJ(newObjMap(vm))
+}
+
+//objMap[_]：返回map[key]对应的值
+static bool primMapSubScript(VM *vm, Value *args) {
+    //校验key的合法性
+    if (!validateKey(vm, args[1]))
+        return false; //出错，切换线程
+
+    //获得map对象实例
+    ObjMap *objMap = VALUE_TO_OBJMAP(args[0]);
+
+    //从map中查找key(args[1])对应的value
+    Value value = mapGet(objMap, args[1]);
+
+    //若没有相应的key则返回NULL
+    if (VALUE_IS_UNDEFINED(value))
+        RET_NULL
+    RET_VALUE(value)
+}
+
+//objMap[_]=(_)：map[key]=value
+static bool primMapSubScriptSetter(VM *vm, Value *args) {
+    //校验key的合法性
+    if (!validateKey(vm, args[1]))
+        return false; //出错，切换线程
+
+    //获得map对象实例
+    ObjMap *objMap = VALUE_TO_OBJMAP(args[0]);
+
+    //map[key]=value
+    mapSet(vm, objMap, args[1], args[2]);
+    RET_VALUE(args[2]) //返回value
+}
+
+//objMap.addCore_(_,_)：编译器编译map字面量时内部使用的, 在map中添加(key-value)对儿并返回map自身
+static bool primMapAddCore(VM *vm, Value *args) {
+    if (!validateKey(vm, args[1]))
+        return false;
+
+    //获得map对象实例
+    ObjMap *objMap = VALUE_TO_OBJMAP(args[0]);
+
+    //map[key]=value
+    mapSet(vm, objMap, args[1], args[2]);
+
+    RET_VALUE(args[0]) //返回map对象
+}
+
+//objMap.clear()：清除map
+static bool primMapClear(VM *vm, Value *args) {
+    clearMap(vm, VALUE_TO_OBJMAP(args[0]));
+    RET_NULL
+}
+
+//objMap.containsKey(_)：判断map是否包含key
+static bool primMapContainsKey(VM *vm, Value *args) {
+    if (!validateKey(vm, args[1]))
+        return false;
+
+    //直接去get该key，判断是否成功
+    RET_BOOL(!VALUE_IS_UNDEFINED(mapGet(VALUE_TO_OBJMAP(args[0]), args[1])))
+}
+
+//objMap.count：返回map中entry个数
+static bool primMapCount(VM *vm UNUSED, Value *args) {
+    RET_NUM(VALUE_TO_OBJMAP(args[0])->count)
+}
+
+//objMap.remove(_)：删除map[key]
+static bool primMapRemove(VM *vm, Value *args) {
+    if (!validateKey(vm, args[1]))
+        return false;
+    RET_VALUE(removeKey(vm, VALUE_TO_OBJMAP(args[0]), args[1]))
+}
+
+//objMap.iterate_(_)：迭代map中的entry，返回entry的索引供keyIteratorValue_和valueIteratorValue_做迭代器
+static bool primMapIterate(VM *vm, Value *args) {
+    //获得map对象实例
+    ObjMap *objMap = VALUE_TO_OBJMAP(args[0]);
+
+    //map中若空则返回false，不可迭代
+    if (objMap->count == 0)
+        RET_FALSE
+
+    //若没有传入迭代器，迭代默认是从第0个entry开始
+    uint32_t index = 0;
+
+    //若不是第一次迭代，传进了迭代器
+    if (!VALUE_IS_NULL(args[1])) {
+        //iter必须是整数
+        if (!validateInt(vm, args[1]))
+            return false;
+
+        //迭代器不能小于0
+        if (VALUE_TO_NUM(args[1]) < 0)
+            RET_FALSE
+
+        index = (uint32_t) VALUE_TO_NUM(args[1]);
+        //迭代器不越界
+        if (index >= objMap->capacity)
+            RET_FALSE
+        index++; //更新迭代器
+    }
+
+    //返回下一个正在使用（有效）的entry
+    for (; index < objMap->capacity; index++)
+        //entries是个数组，元素是哈希槽，哈希值散布在这些槽中，不连续，因此逐个判断槽位是否在用
+        if (!VALUE_IS_UNDEFINED(objMap->entries[index].key))
+            RET_NUM(index) //返回entry索引
+
+    //若没有有效的entry了就返回false，迭代结束
+    RET_FALSE
+}
+
+//objMap.keyIteratorValue_(_)：key=map.keyIteratorValue(iter)
+static bool primMapKeyIteratorValue(VM *vm, Value *args) {
+    ObjMap *objMap = VALUE_TO_OBJMAP(args[0]);
+
+    uint32_t index = validateIndex(vm, args[1], objMap->capacity);
+    if (index == UINT32_MAX)
+        return false;
+
+    Entry *entry = &objMap->entries[index];
+    if (VALUE_IS_UNDEFINED(entry->key))
+        SET_ERROR_FALSE(vm, "invalid iterator.")
+
+    //返回该key
+    RET_VALUE(entry->key)
+}
+
+//objMap.valueIteratorValue_(_)：value=map.valueIteratorValue(iter)
+static bool primMapValueIteratorValue(VM *vm, Value *args) {
+    ObjMap *objMap = VALUE_TO_OBJMAP(args[0]);
+
+    uint32_t index = validateIndex(vm, args[1], objMap->capacity);
+    if (index == UINT32_MAX)
+        return false;
+
+    Entry *entry = &objMap->entries[index];
+    if (VALUE_IS_UNDEFINED(entry->key))
+        SET_ERROR_FALSE(vm, "invalid iterator.")
+
+    //返回该value
+    RET_VALUE(entry->value)
+}
+
 //从modules中获取名为moduleName的模块
 static ObjModule *getModule(VM *vm, Value moduleName) {
     Value value = mapGet(vm->allModules, moduleName);
@@ -1329,4 +1493,18 @@ void buildCore(VM *vm) {
     PRIM_METHOD_BIND(vm->listClass, "iterate(_)", primListIterate)
     PRIM_METHOD_BIND(vm->listClass, "iteratorValue(_)", primListIteratorValue)
     PRIM_METHOD_BIND(vm->listClass, "remove(_)", primListRemove)
+
+    //绑定Map类
+    vm->mapClass = VALUE_TO_CLASS(getCoreClassValue(coreModule, "Map"));
+    PRIM_METHOD_BIND(vm->mapClass->objHeader.class, "new()", primMapNew)
+    PRIM_METHOD_BIND(vm->mapClass, "[_]", primMapSubScript)
+    PRIM_METHOD_BIND(vm->mapClass, "[_]=(_)", primMapSubScriptSetter)
+    PRIM_METHOD_BIND(vm->mapClass, "addCore_(_,_)", primMapAddCore)
+    PRIM_METHOD_BIND(vm->mapClass, "clear()", primMapClear)
+    PRIM_METHOD_BIND(vm->mapClass, "containsKey(_)", primMapContainsKey)
+    PRIM_METHOD_BIND(vm->mapClass, "count", primMapCount)
+    PRIM_METHOD_BIND(vm->mapClass, "remove(_)", primMapRemove)
+    PRIM_METHOD_BIND(vm->mapClass, "iterate_(_)", primMapIterate)
+    PRIM_METHOD_BIND(vm->mapClass, "keyIteratorValue_(_)", primMapKeyIteratorValue)
+    PRIM_METHOD_BIND(vm->mapClass, "valueIteratorValue_(_)", primMapValueIteratorValue)
 }
